bfbdead174ae75bcb306d7846a3220be9443b8be
mirea <mirea@DESKTOP-MTHJB84>
2022-03-04 11:10:58 +0300

    Добавьте файлы проекта.

---
+++ array2/array2.cpp
@@ -0,0 +1,29 @@
+﻿
+#include <iostream>
+using namespace std;
+void Input(int a[][], int n, int m)
+{
+    cout << "Enter " << n << " x " << m << " numbers\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+    }
+}
+void Output(int a[][], int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+            cout >> a[i][j];
+    }
+}
+int main()
+{
+    int a[100][100];
+    cout << "Enter NxM size of array\n";
+    int n,m;
+    cin >> n >> m;
+
+}
+
